bb9f4bf8bbc8284b33bc29a056c65b32c9d662b4
Tom Nightingale <tom@weareleague.co.uk>
2021-10-14 19:16:25 +0100

    update global and home schema

--- schemas/singletonGlobal.js
+++ schemas/singletonGlobal.js
@@ -8,6 +8,51 @@ export default {
             title: 'Title',
             name: 'title',
             type: 'string',
+            initialValue: 'Global Settings',
+            readOnly: true,
+            hidden: true,
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Address',
+            description: 'The main Excelsior address',
+            name: 'address',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Phone Number',
+            description: 'The main Excelsior phone number',
+            name: 'phoneNumber',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Email Address',
+            description: 'The main Excelsior email address',
+            name: 'emailAddress',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Youtube URL',
+            description: 'The Excelsior Youtube URL',
+            name: 'youtubeUrl',
+            type: 'url',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Facebook URL',
+            description: 'The Excelsior Facebook URL',
+            name: 'facebookUrl',
+            type: 'url',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Instagram URL',
+            description: 'The Excelsior Instagram URL',
+            name: 'instagramUrl',
+            type: 'url',
             validation: Rule => Rule.required()
         },
     ],
--- schemas/singletonHome.js
+++ schemas/singletonHome.js
@@ -8,21 +8,64 @@ export default {
             title: 'Title',
             name: 'title',
             type: 'string',
+            initialValue: 'Homepage Settings',
+            readOnly: true,
+            hidden: true,
             validation: Rule => Rule.required()
         },
         {
-            title: 'Content Heading',
-            name: 'contentHeading',
+            title: 'Hero Images',
+            description: 'The images that appear as carousel in the hero section',
+            name: 'heroImages',
+            type: 'array',
+            options: {
+                layout: 'grid'
+            },
+            of: [{
+                type: 'image'
+            }],
+            validation: Rule => Rule.min(1)
+        },
+        {
+            title: 'Hero Sub Heading',
+            name: 'heroSubHeading',
             type: 'string',
             validation: Rule => Rule.required()
         },
         {
-            title: 'Content',
-            name: 'content',
+            title: 'Hero Heading',
+            name: 'heroHeading',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'Hero Blurb',
+            name: 'heroBlurb',
+            type: 'text',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'About Content Heading',
+            description: 'The heading for the "About Excelsior" section',
+            name: 'aboutContentHeading',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
+        {
+            title: 'About Content',
+            description: 'The content for the "About Excelsior" section',
+            name: 'aboutContent',
             type: 'array', 
             of: [{type: 'block'}],
             validation: Rule => Rule.required()
         },        
+        {
+            title: 'Inspirational Quote',
+            description: 'The quote shown before the teacher introductions',
+            name: 'inspirationalQuote',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
         {
             title: 'SEO / Share Settings',
             name: 'seo',
