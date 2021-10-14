f5054d8a95e69b93ec1fed289da66ba86896d803
Tom Nightingale <tom@weareleague.co.uk>
2021-10-14 18:12:14 +0100

    add sanity core files

---
+++ sanity-structure.js
@@ -0,0 +1,43 @@
+import S from "@sanity/desk-tool/structure-builder";
+import IframePreview from './preview/IFramePreview'
+
+import {
+  FiHome,
+  FiAlertCircle,
+  FiAlignLeft,
+  FiBox,
+  FiCornerDownLeft,
+  FiClipboard
+} from 'react-icons/fi'
+
+import { getGlobalSlug, previewURL } from './utils/resolveProductionUrl'
+
+export const getDefaultDocumentNode = ({ schemaType }) => S.document().views(getPreview(schemaType))
+
+const getPreview = (schemaType) => {
+  const globalSlug = getGlobalSlug(schemaType)
+  if (globalSlug) {
+    return [
+      S.view.form(),
+      S.view
+        .component(IframePreview)
+        .title('Web preview')
+        .options({ previewURL, isMobile: false, globalSlug }),
+      S.view
+        .component(IframePreview)
+        .title('Mobile preview')
+        .options({ previewURL, isMobile: true, globalSlug })
+    ]
+  }
+  return [S.view.form()]
+}
+
+export default () =>
+  S.list()
+    .title("Content")
+    .items([
+      S.listItem().title('Global').child(S.editor().id('global').schemaType('global').documentId('global').views(getPreview('global'))).icon(FiAlertCircle),
+      S.divider(),
+      S.listItem().title('Home').child(S.editor().id('home').schemaType('home').documentId('home').views(getPreview('home'))).icon(FiHome),
+      S.divider(),
+    ]);
---
+++ schemas/singletonGlobal.js
@@ -0,0 +1,25 @@
+export default {
+    title: 'Global Settings',
+    name: 'global',
+    type: 'document',
+    __experimental_actions: ['update', 'create', 'delete', 'publish'],
+    fields: [
+        {
+            title: 'Title',
+            name: 'title',
+            type: 'string',
+            validation: Rule => Rule.required()
+        },
+    ],
+    preview: {
+        select: {
+            title: 'title'
+        },
+        prepare({ title }) {
+            return {
+                title
+            }
+        }
+    }
+}
+
